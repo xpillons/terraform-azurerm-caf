5fec0764b3be21a88aa4a4ad8aaa9ce2edca47b7
Xavier Pillons <xpillons@microsoft.com>
2020-12-15 12:14:00 +0000

    First end to end implementation for NFSv3

--- modules/netapp/output.tf
+++ modules/netapp/output.tf
@@ -1,7 +1,3 @@
-output id {
-  value = azurerm_netapp_account.account.id
-}
-
 output pools {
-  value = azurerm_netapp_pool.pool
+  value = module.pools
 }
--- modules/netapp/pool/variables.tf
+++ modules/netapp/pool/variables.tf
@@ -3,7 +3,9 @@ variable client_config {}
 variable settings {}
 variable resource_group_name {}
 variable location {}
+variable account_name {}
+variable vnets {}
+
 variable base_tags {
   default = {}
 }
-variable vnets {}
